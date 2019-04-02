aba196e36a8a06924cb674fa41109628e7b19b8e
Harald Fosen Grunnaleite <haraldfosengrunnaleite@Haralds-MacBook-Pro.local>
2019-04-02 13:48:56 +0200

    butikk.js

---
+++ butikk.js
@@ -0,0 +1,52 @@
+// @ts-check
+
+function setup() {
+    let select = document.querySelector("db-select");
+    let form = document.querySelector("db-form");
+    let homebar = document.querySelector('home-bar');
+    let refreshList = Array.from(document.querySelectorAll(".db-refresh"));
+    if (homebar) {
+        homebar.setAttribute("menu",
+            `<i class="material-icons">menu</i>
+        <ul>
+          <li>Kunde
+          <li>Vare
+          <li>Butikk
+          <li>Korg
+          <li>Bestilling
+          <li>Linje
+        </ul>
+        `)
+        homebar.addEventListener("menu", menuHandler);
+    }
+    if (select) {
+        select.addEventListener("korg", bestilling);
+        select.addEventListener("slett", slettKunder);
+    }
+
+    if (form) {
+        form.addEventListener("saved", refresh);
+    }
+
+    function refresh() {
+        setTimeout( () => refreshList.forEach(e => e.getData()) , 500);
+    }
+
+    function slettKunder() {
+        // fra kundeskjema
+        alert("Sletter valgte kunder");
+    }
+
+    function bestilling() {
+        // fra butikk-skjema
+        location.href = "korg.html";
+    }
+
+    function menuHandler(e) {
+        let info = homebar.info;
+        let text = info.target.innerHTML.trim().toLowerCase();
+        if (text) {
+            location.href = text + ".html";
+        }
+    }
+}
