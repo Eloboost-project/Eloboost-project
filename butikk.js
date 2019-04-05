2bcbf5007e1c90105278ba7c5189ad404df6fcce
Harald Fosen Grunnaleite <haraldfosengrunnaleite@Haralds-MacBook-Pro.local>
2019-04-05 11:35:39 +0200

    butikk.js

--- butikk.js
+++ butikk.js
@@ -7,7 +7,7 @@ function setup() {
     let refreshList = Array.from(document.querySelectorAll(".db-refresh"));
     if (homebar) {
         homebar.setAttribute("menu",
-            `<i class="material-icons">Game</i>
+            `<i class="material-icons">Home</i>
         <ul>
           <li>LOL
           <li>CSGO
